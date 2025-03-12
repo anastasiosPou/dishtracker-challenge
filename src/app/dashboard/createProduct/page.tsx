16c07b5083557f270be371385fa2235e8470beb7
Anastasios Poursaitidis <tasos.poursaitidis.4@gmail.com>
2025-03-12 09:22:56 +0200

    Create the createProduct route/page

---
+++ src/app/dashboard/createProduct/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+
+/*
+  The CreateProductPage component will render the CreateProduct component.
+*/
+import CreateProduct from "../../../components/createProduct/CreateProduct";
+
+export default function CreateProductPage() {
+  return <section className="createProductPage"><CreateProduct /></section>
+}
